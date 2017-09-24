a847c34e333a2f4a5dec39e534b636953fd92121
JNSMAR043 <JNSMAR043@myuct.ac.za>
2017-09-24 12:55:21 +0200

    second commit

--- WebApplication1/WebApplication1/App_Start/FilterConfig.cs
+++ WebApplication1/WebApplication1/App_Start/FilterConfig.cs
@@ -5,6 +5,10 @@ namespace WebApplication1
 {
     public class FilterConfig
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="filters"></param>
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
